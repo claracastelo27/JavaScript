96f41e573930a56c34d7ee2ff2265b8fecd4bae4
claracastelo27 <124395511+claracastelo27@users.noreply.github.com>
2025-04-28 07:54:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-function insertToDisplay(data){
-    document.querySelector('#display').value += data
-}
+const formularioLogin = document.getElementById('formulario-login');
+        const formularioCadastro = document.getElementById('formulario-cadastro');
+        const mostrarCadastroBotao = document.getElementById('mostrar-cadastro');
+        const mostrarLoginBotao = document.getElementById('mostrar-login');
+        const corpo = document.body;
+        const botoesAlternar = document.querySelectorAll('.container-botao-alternar');
 
-function clean(){
-    document.querySelector('#display').value = ''
-}
+        function mudarCor(cor) {
+            corpo.style.backgroundColor = cor;
+        }
 
-function back(){
-    const display = document.querySelector('#display')
-    display.value = display.value.slice(0, -1)
-}
-function result(){
-    const display = document.querySelector('#display')
-    try {
-        display.value = eval(display.value)
-    } catch {
-        display.value = 'Error'
-    }
-}
+        mostrarCadastroBotao.addEventListener('click', () => {
+            formularioLogin.classList.remove('ativo');
+            formularioCadastro.classList.add('ativo');
+            mudarCor('#81d4fa'); 
+        });
+
+        mostrarLoginBotao.addEventListener('click', () => {
+            formularioCadastro.classList.remove('ativo');
+            formularioLogin.classList.add('ativo');
+            mudarCor('#e0f7fa'); 
+        });
+
+    
+        mudarCor('#e0f7fa');
